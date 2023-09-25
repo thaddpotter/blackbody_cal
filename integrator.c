ff31433ee03d2a6fb3f3f1b73244cd2a2e1b9be8
thaddpotter <62678803+thaddpotter@users.noreply.github.com>
2023-09-25 08:58:40 -0400

    updates

--- integrator.c
+++ integrator.c
@@ -1,19 +1,15 @@
 #include "integrator.h"
 
-double rfunc(double coeff[10], double nu){
-    //Set up as a LC of orthogonal set of polynomials
-    
-    //
-
-
-    return 1;
+double rfunc(double coeff[7], double nu){
+    //Step 0: Just make it a power series:
+    return coeff[0] + coeff[1]*nu + coeff[2]*nu*nu + coeff[3]*nu*nu*nu + coeff[4]*pow(nu,4) + coeff[5]*pow(nu,5) + coeff[6]*pow(nu,6) + coeff[7]*pow(nu,7);
 }
 
-double integrand(double nu, double T, double coeff[10]){
+double integrand(double nu, double T, double coeff[7]){
     return (nu*nu*nu)/(exp(nu)-1)* rfunc(coeff, nu);
 }
 
-double integrator(double lowerlim, double upperlim, double errtol, double T,double coeff[10]){
+double integrator(double lowerlim, double upperlim, double errtol, double T,double coeff[7]){
 
     //Setup
     double rough_ans = 0;
@@ -31,12 +27,9 @@ double integrator(double lowerlim, double upperlim, double errtol, double T,doub
     for (int i=0; i<21; i++){
         ans += C*wi2[i]*integrand((25*kb*T/h)*(xi2[i]+1),T,coeff);
     }
-
-    //Error Estimate
-    double err = abs(ans-rough_ans);
     
     //If error tolerance not met, then split the interval and recurse
-    if ( (err/(upperlim-lowerlim)) > errtol){
+    if ( (abs(ans-rough_ans)/(upperlim-lowerlim)) > errtol){
         double mid = 0.5*(upperlim-lowerlim);
         ans = integrator(lowerlim,mid,errtol,T,coeff) + integrator(mid,upperlim,errtol,T,coeff);
     }
